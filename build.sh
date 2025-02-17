ef5084723bb32ae0e362b28f8230abaec8a10a61
miya264 <tmiyajima117@gmail.com>
2025-02-17 10:33:49 +0900

    修正

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-# Render の環境では root 権限がないため sudo を使う
-sudo apt-get update -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false
-sudo apt-get install -y portaudio19-dev
-
-# poetry を使っている場合
-poetry install
-=======
 # 環境のパッケージリストを更新
 apt-get update -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false
 apt-get install -y portaudio19-dev libasound2-dev
@@ -19,4 +11,4 @@ else
     echo "Error: pyproject.toml not found"
     exit 1
 fi
->>>>>>> 39b1ce9 (修正)
+
